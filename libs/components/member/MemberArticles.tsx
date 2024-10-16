77d979524de8a479b7e081148834e64cf9e32332
Tillochik01 <xuribayevzakaryo@gmail.com>
2024-10-17 00:28:13 +0900

    BBR: Initial sample

---
+++ libs/components/member/MemberArticles.tsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState } from 'react';
+import { NextPage } from 'next';
+import { Pagination, Stack, Typography } from '@mui/material';
+import useDeviceDetect from '../../hooks/useDeviceDetect';
+import { useRouter } from 'next/router';
+import CommunityCard from '../common/CommunityCard';
+import { T } from '../../types/common';
+import { BoardArticle } from '../../types/board-article/board-article';
+import { BoardArticlesInquiry } from '../../types/board-article/board-article.input';
+import { GET_BOARD_ARTICLES } from '../../../apollo/user/query';
+import { useMutation, useQuery } from '@apollo/client';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../sweetAlert';
+import { LIKE_TARGET_BOARD_ARTICLE } from '../../../apollo/user/mutation';
+import { Messages } from '../../config';
+
+const MemberArticles: NextPage = ({ initialInput, ...props }: any) => {
+	const device = useDeviceDetect();
+	const router = useRouter();
+	const [total, setTotal] = useState<number>(0);
+	const { memberId } = router.query;
+	const [searchFilter, setSearchFilter] = useState<BoardArticlesInquiry>(initialInput);
+	const [memberBoArticles, setMemberBoArticles] = useState<BoardArticle[]>([]);
+
+	/** APOLLO REQUESTS **/
+	const [likeTargetBoardArticle] = useMutation(LIKE_TARGET_BOARD_ARTICLE);
+
+	const {
+		loading: getBoardArticlesLoading,
+		data: getBoardArticlesData,
+		error: getBoardArticlesError,
+		refetch: getBoardArticlesRefetch,
+	} = useQuery(GET_BOARD_ARTICLES, {
+		fetchPolicy: 'network-only',
+		variables: { input: searchFilter },
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: any) => {
+			setMemberBoArticles(data?.getBoardArticles?.list);
+			setTotal(data?.getBoardArticles?.metaCounter[0]?.total ?? 0);
+		},
+	});
+
+	/** LIFECYCLES **/
+	useEffect(() => {
+		if (memberId) setSearchFilter({ ...initialInput, search: { memberId: memberId } });
+	}, [memberId]);
+
+	/** HANDLERS **/
+	const paginationHandler = (e: T, value: number) => {
+		setSearchFilter({ ...searchFilter, page: value });
+	};
+
+	const likeArticleHandler = async (e: any, user: any, id: string) => {
+		try {
+			e.stopPropagation();
+			if (!id) return;
+			if (!user._id) throw new Error(Messages.error2);
+
+			await likeTargetBoardArticle({
+				variables: {
+					input: id,
+				},
+			});
+			await getBoardArticlesRefetch({ input: searchFilter });
+			await sweetTopSmallSuccessAlert('success', 800);
+		} catch (err: any) {
+			console.log('ERROR, likeArticleHandler', err.message);
+			await sweetMixinErrorAlert(err.message).then();
+		}
+	};
+
+	if (device === 'mobile') {
+		return <div>MEMBER ARTICLES MOBILE</div>;
+	} else {
+		return (
+			<div id="member-articles-page">
+				<Stack className="main-title-box">
+					<Stack className="right-box">
+						<Typography className="main-title">Articles</Typography>
+					</Stack>
+				</Stack>
+				<Stack className="articles-list-box">
+					{memberBoArticles?.length === 0 && (
+						<div className={'no-data'}>
+							<img src="/img/icons/icoAlert.svg" alt="" />
+							<p>No Articles found!</p>
+						</div>
+					)}
+					{memberBoArticles?.map((boardArticle: BoardArticle) => {
+						return (
+							<CommunityCard
+								boardArticle={boardArticle}
+								key={boardArticle?._id}
+								size={'small'}
+								likeArticleHandler={likeArticleHandler}
+							/>
+						);
+					})}
+				</Stack>
+				{memberBoArticles?.length !== 0 && (
+					<Stack className="pagination-config">
+						<Stack className="pagination-box">
+							<Pagination
+								count={Math.ceil(total / searchFilter.limit) || 1}
+								page={searchFilter.page}
+								shape="circular"
+								color="primary"
+								onChange={paginationHandler}
+							/>
+						</Stack>
+						<Stack className="total-result">
+							<Typography>{total} property available</Typography>
+						</Stack>
+					</Stack>
+				)}
+			</div>
+		);
+	}
+};
+
+MemberArticles.defaultProps = {
+	initialInput: {
+		page: 1,
+		limit: 6,
+		sort: 'createdAt',
+		direction: 'DESC',
+		search: {},
+	},
+};
+
+export default MemberArticles;
---
+++ pages/agent/index.tsx
@@ -0,0 +1,231 @@
+import React, { ChangeEvent, MouseEvent, useEffect, useState } from 'react';
+import { NextPage } from 'next';
+import useDeviceDetect from '../../libs/hooks/useDeviceDetect';
+import withLayoutBasic from '../../libs/components/layout/LayoutBasic';
+import { Stack, Box, Button, Pagination } from '@mui/material';
+import { Menu, MenuItem } from '@mui/material';
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
+import AgentCard from '../../libs/components/common/AgentCard';
+import { useRouter } from 'next/router';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { Member } from '../../libs/types/member/member';
+import { useMutation, useQuery } from '@apollo/client';
+import { LIKE_TARGET_MEMBER } from '../../apollo/user/mutation';
+import { GET_AGENTS } from '../../apollo/user/query';
+import { T } from '../../libs/types/common';
+import {
+	sweetErrorHandling,
+	sweetMixinErrorAlert,
+	sweetMixinSuccessAlert,
+	sweetTopSmallSuccessAlert,
+} from '../../libs/sweetAlert';
+import { Messages } from '../../libs/config';
+
+export const getStaticProps = async ({ locale }: any) => ({
+	props: {
+		...(await serverSideTranslations(locale, ['common'])),
+	},
+});
+
+const AgentList: NextPage = ({ initialInput, ...props }: any) => {
+	const device = useDeviceDetect();
+	const router = useRouter();
+	const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
+	const [filterSortName, setFilterSortName] = useState('Recent');
+	const [sortingOpen, setSortingOpen] = useState(false);
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const [searchFilter, setSearchFilter] = useState<any>(
+		router?.query?.input ? JSON.parse(router?.query?.input as string) : initialInput,
+	);
+	const [agents, setAgents] = useState<Member[]>([]);
+	const [total, setTotal] = useState<number>(0);
+	const [currentPage, setCurrentPage] = useState<number>(1);
+	const [searchText, setSearchText] = useState<string>('');
+
+	/** APOLLO REQUESTS **/
+
+	const [likeTArgetMember] = useMutation(LIKE_TARGET_MEMBER);
+	const {
+		loading: getAgentLoading,
+		data: getAgentData,
+		error: getAgentError,
+		refetch: getAgentsRefetch,
+	} = useQuery(GET_AGENTS, {
+		fetchPolicy: 'cache-and-network',
+		variables: {
+			input: searchFilter,
+		},
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			setAgents(data?.getAgents?.list);
+			setTotal(data?.getAgents?.metaCounter[0]?.total);
+		},
+	});
+
+	/** LIFECYCLES **/
+	useEffect(() => {
+		if (router.query.input) {
+			const input_obj = JSON.parse(router?.query?.input as string);
+			setSearchFilter(input_obj);
+		} else
+			router.replace(`/agent?input=${JSON.stringify(searchFilter)}`, `/agent?input=${JSON.stringify(searchFilter)}`);
+
+		setCurrentPage(searchFilter.page === undefined ? 1 : searchFilter.page);
+	}, [router]);
+
+	/** HANDLERS **/
+	const sortingClickHandler = (e: MouseEvent<HTMLElement>) => {
+		setAnchorEl(e.currentTarget);
+		setSortingOpen(true);
+	};
+
+	const sortingCloseHandler = () => {
+		setSortingOpen(false);
+		setAnchorEl(null);
+	};
+
+	const sortingHandler = (e: React.MouseEvent<HTMLLIElement>) => {
+		switch (e.currentTarget.id) {
+			case 'recent':
+				setSearchFilter({ ...searchFilter, sort: 'createdAt', direction: 'DESC' });
+				setFilterSortName('Recent');
+				break;
+			case 'old':
+				setSearchFilter({ ...searchFilter, sort: 'createdAt', direction: 'ASC' });
+				setFilterSortName('Oldest order');
+				break;
+			case 'likes':
+				setSearchFilter({ ...searchFilter, sort: 'memberLikes', direction: 'DESC' });
+				setFilterSortName('Likes');
+				break;
+			case 'views':
+				setSearchFilter({ ...searchFilter, sort: 'memberViews', direction: 'DESC' });
+				setFilterSortName('Views');
+				break;
+		}
+		setSortingOpen(false);
+		setAnchorEl2(null);
+	};
+
+	const paginationChangeHandler = async (event: ChangeEvent<unknown>, value: number) => {
+		searchFilter.page = value;
+		await router.push(`/agent?input=${JSON.stringify(searchFilter)}`, `/agent?input=${JSON.stringify(searchFilter)}`, {
+			scroll: false,
+		});
+		setCurrentPage(value);
+	};
+
+	const likeMemberHandler = async (user: any, id: string) => {
+		try {
+			if (!id) return;
+			if (!user._id) throw new Error(Messages.error2);
+
+			await likeTArgetMember({
+				variables: {
+					input: id,
+				},
+			});
+			sweetTopSmallSuccessAlert('success', 800);
+			await getAgentsRefetch({ input: searchFilter });
+		} catch (err: any) {
+			console.log('Error on likeMember', err);
+			sweetMixinErrorAlert(err.message).then();
+		}
+	};
+
+	if (device === 'mobile') {
+		return <h1>AGENTS PAGE MOBILE</h1>;
+	} else {
+		return (
+			<Stack className={'agent-list-page'}>
+				<Stack className={'container'}>
+					<Stack className={'filter'}>
+						<Box component={'div'} className={'left'}>
+							<input
+								type="text"
+								placeholder={'Search for an agent'}
+								value={searchText}
+								onChange={(e: any) => setSearchText(e.target.value)}
+								onKeyDown={(event: any) => {
+									if (event.key == 'Enter') {
+										setSearchFilter({
+											...searchFilter,
+											search: { ...searchFilter.search, text: searchText },
+										});
+									}
+								}}
+							/>
+						</Box>
+						<Box component={'div'} className={'right'}>
+							<span>Sort by</span>
+							<div>
+								<Button onClick={sortingClickHandler} endIcon={<KeyboardArrowDownRoundedIcon />}>
+									{filterSortName}
+								</Button>
+								<Menu anchorEl={anchorEl} open={sortingOpen} onClose={sortingCloseHandler} sx={{ paddingTop: '5px' }}>
+									<MenuItem onClick={sortingHandler} id={'recent'} disableRipple>
+										Recent
+									</MenuItem>
+									<MenuItem onClick={sortingHandler} id={'old'} disableRipple>
+										Oldest
+									</MenuItem>
+									<MenuItem onClick={sortingHandler} id={'likes'} disableRipple>
+										Likes
+									</MenuItem>
+									<MenuItem onClick={sortingHandler} id={'views'} disableRipple>
+										Views
+									</MenuItem>
+								</Menu>
+							</div>
+						</Box>
+					</Stack>
+					<Stack className={'card-wrap'}>
+						{agents?.length === 0 ? (
+							<div className={'no-data'}>
+								<img src="/img/icons/icoAlert.svg" alt="" />
+								<p>No Agents found!</p>
+							</div>
+						) : (
+							agents.map((agent: Member) => {
+								return <AgentCard likeMemberHandler={likeMemberHandler} agent={agent} key={agent._id} />;
+							})
+						)}
+					</Stack>
+					<Stack className={'pagination'}>
+						<Stack className="pagination-box">
+							{agents.length !== 0 && Math.ceil(total / searchFilter.limit) > 1 && (
+								<Stack className="pagination-box">
+									<Pagination
+										page={currentPage}
+										count={Math.ceil(total / searchFilter.limit)}
+										onChange={paginationChangeHandler}
+										shape="circular"
+										color="primary"
+									/>
+								</Stack>
+							)}
+						</Stack>
+
+						{agents.length !== 0 && (
+							<span>
+								Total {total} agent{total > 1 ? 's' : ''} available
+							</span>
+						)}
+					</Stack>
+				</Stack>
+			</Stack>
+		);
+	}
+};
+
+AgentList.defaultProps = {
+	initialInput: {
+		page: 1,
+		limit: 10,
+		sort: 'createdAt',
+		direction: 'DESC',
+		search: {},
+	},
+};
+
+export default withLayoutBasic(AgentList);
