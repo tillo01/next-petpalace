2101617c1cc7754c605cfc1b0f7db067698f76a4
Tillochik01 <xuribayevzakaryo@gmail.com>
2024-11-25 23:36:12 +0900

    feat: fix problems

--- apollo/admin/query.ts
+++ apollo/admin/query.ts
@@ -1,8 +1,8 @@
-import { gql } from '@apollo/client';
-
 /**************************
  *         MEMBER         *
  *************************/
+
+import { gql } from '@apollo/client';
 
 export const GET_ALL_MEMBERS_BY_ADMIN = gql`
 	query GetAllMembersByAdmin($input: MembersInquiry!) {
