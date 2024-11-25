ca2e9d95c107c512e1a4b1a66ff6143d1b4ff274
Tillochik01 <xuribayevzakaryo@gmail.com>
2024-11-25 20:46:05 +0900

    fix: fiberContainer has been deleted

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -4,7 +4,6 @@ import Head from 'next/head';
 import Top from '../Top';
 import Footer from '../Footer';
 import { Stack } from '@mui/material';
-import FiberContainer from '../common/FiberContainer';
 import HeaderFilter from '../homepage/HeaderFilter';
 import { userVar } from '../../../apollo/store';
 import { useReactiveVar } from '@apollo/client';
