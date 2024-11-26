1c674ee99357fc990624ca78d53c30726586332d
Tillochik01 <xuribayevzakaryo@gmail.com>
2024-11-27 02:50:52 +0900

    fix: commented add page

--- pages/_admin/cs/add/FAQTeditor.tsx
+++ pages/_admin/cs/add/FAQTeditor.tsx
@@ -1,212 +1,212 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { Box, Button, FormControl, MenuItem, Stack, Typography, Select, TextField } from '@mui/material';
-import { useRouter } from 'next/router';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import dynamic from 'next/dynamic';
-import { useMutation } from '@apollo/client';
-import { CREATE_FAQ_QUESTIONS } from '../../../../apollo/user/mutation';
-import { getJwtToken } from '../../../../libs/auth';
-import { Editor } from '@toast-ui/react-editor';
-import { T } from '../../../../libs/types/common';
-import { Message } from '../../../../libs/enums/common.enum';
-import { sweetErrorHandling, sweetTopSuccessAlert } from '../../../../libs/sweetAlert';
-import { NoticeCategory, NoticeStatus, NoticeType } from '../../../../libs/enums/notice.enum';
+// import React, { useEffect, useMemo, useRef, useState } from 'react';
+// import { Box, Button, FormControl, MenuItem, Stack, Typography, Select, TextField } from '@mui/material';
+// import { useRouter } from 'next/router';
+// import '@toast-ui/editor/dist/toastui-editor.css';
+// import dynamic from 'next/dynamic';
+// import { useMutation } from '@apollo/client';
+// import { CREATE_FAQ_QUESTIONS } from '../../../../apollo/user/mutation';
+// import { getJwtToken } from '../../../../libs/auth';
+// import { Editor } from '@toast-ui/react-editor';
+// import { T } from '../../../../libs/types/common';
+// import { Message } from '../../../../libs/enums/common.enum';
+// import { sweetErrorHandling, sweetTopSuccessAlert } from '../../../../libs/sweetAlert';
+// import { NoticeCategory, NoticeStatus, NoticeType } from '../../../../libs/enums/notice.enum';
 
-const FAQTeditor = () => {
-	const editorRef = useRef<Editor>(null),
-		token = getJwtToken(),
-		router = useRouter();
-	const [noticeCategory, setNoticeCategory] = useState<NoticeCategory>(NoticeCategory.NOTICE);
-	const [noticeStatus, setNoticeStatus] = useState<NoticeStatus>(NoticeStatus.HOLD);
-	const [noticeType, setNoticeType] = useState<NoticeType>(NoticeType.PET);
-	const [EditorComponent, setEditorComponent] = useState<React.ComponentType | null>(null);
+// const FAQTeditor = () => {
+// 	const editorRef = useRef<Editor>(null),
+// 		token = getJwtToken(),
+// 		router = useRouter();
+// 	const [noticeCategory, setNoticeCategory] = useState<NoticeCategory>(NoticeCategory.NOTICE);
+// 	const [noticeStatus, setNoticeStatus] = useState<NoticeStatus>(NoticeStatus.HOLD);
+// 	const [noticeType, setNoticeType] = useState<NoticeType>(NoticeType.PET);
+// 	const [EditorComponent, setEditorComponent] = useState<React.ComponentType | null>(null);
 
-	useEffect(() => {
-		const initTerminal = async () => {
-			const { Editor } = await import('@toast-ui/react-editor');
-		};
-		initTerminal();
-	}, []);
+// 	useEffect(() => {
+// 		const initTerminal = async () => {
+// 			const { Editor } = await import('@toast-ui/react-editor');
+// 		};
+// 		initTerminal();
+// 	}, []);
 
-	/** APOLLO REQUESTS **/
-	const [createFaqQuestions] = useMutation(CREATE_FAQ_QUESTIONS);
+// 	/** APOLLO REQUESTS **/
+// 	const [createFaqQuestions] = useMutation(CREATE_FAQ_QUESTIONS);
 
-	const memoizedValues = useMemo(() => {
-		const noticeTitle = '',
-			noticeContent = '';
+// 	const memoizedValues = useMemo(() => {
+// 		const noticeTitle = '',
+// 			noticeContent = '';
 
-		return { noticeTitle, noticeContent };
-	}, []);
+// 		return { noticeTitle, noticeContent };
+// 	}, []);
 
-	/** HANDLERS **/
+// 	/** HANDLERS **/
 
-	const changeCategoryHandler = (e: any) => {
-		setNoticeCategory(e.target.value);
-	};
-	const changeStatusHandler = (e: any) => {
-		setNoticeStatus(e.target.value);
-	};
-	const changeTypeHandler = (e: any) => {
-		setNoticeType(e.target.value);
-	};
+// 	const changeCategoryHandler = (e: any) => {
+// 		setNoticeCategory(e.target.value);
+// 	};
+// 	const changeStatusHandler = (e: any) => {
+// 		setNoticeStatus(e.target.value);
+// 	};
+// 	const changeTypeHandler = (e: any) => {
+// 		setNoticeType(e.target.value);
+// 	};
 
-	const articleTitleHandler = (e: T) => {
-		console.log(e.target.value);
-		memoizedValues.noticeTitle = e.target.value;
-	};
+// 	const articleTitleHandler = (e: T) => {
+// 		console.log(e.target.value);
+// 		memoizedValues.noticeTitle = e.target.value;
+// 	};
 
-	const doDisabledCheck = () => {
-		if (memoizedValues.noticeContent === '' || memoizedValues.noticeTitle === '') {
-			return true;
-		}
-	};
-	const handleRegisterButton = async () => {
-		try {
-			const editor = editorRef.current;
-			const noticeContent = editor?.getInstance().getHTML() as string;
+// 	const doDisabledCheck = () => {
+// 		if (memoizedValues.noticeContent === '' || memoizedValues.noticeTitle === '') {
+// 			return true;
+// 		}
+// 	};
+// 	const handleRegisterButton = async () => {
+// 		try {
+// 			const editor = editorRef.current;
+// 			const noticeContent = editor?.getInstance().getHTML() as string;
 
-			memoizedValues.noticeContent = noticeContent;
+// 			memoizedValues.noticeContent = noticeContent;
 
-			if (memoizedValues.noticeContent === '' || memoizedValues.noticeTitle === '') {
-				throw new Error(Message.INSERT_ALL_INPUTS);
-			}
+// 			if (memoizedValues.noticeContent === '' || memoizedValues.noticeTitle === '') {
+// 				throw new Error(Message.INSERT_ALL_INPUTS);
+// 			}
 
-			await createFaqQuestions({
-				variables: {
-					input: { ...memoizedValues, noticeCategory, noticeStatus, noticeType },
-				},
-			});
+// 			await createFaqQuestions({
+// 				variables: {
+// 					input: { ...memoizedValues, noticeCategory, noticeStatus, noticeType },
+// 				},
+// 			});
 
-			await sweetTopSuccessAlert(`${noticeCategory === 'FAQ' ? 'FAQ' : 'NOTICE'} is created successfully`, 700);
+// 			await sweetTopSuccessAlert(`${noticeCategory === 'FAQ' ? 'FAQ' : 'NOTICE'} is created successfully`, 700);
 
-			await router.push({
-				pathname: noticeCategory === 'FAQ' ? '/_admin/cs/faq' : '/_admin/cs/notice',
-				query: {
-					category: noticeCategory === 'FAQ' ? '/_admin/cs/faq' : ' /_admin/cs/notice',
-				},
-			});
-		} catch (err: any) {
-			console.log(err);
-			sweetErrorHandling(new Error(Message.INSERT_ALL_INPUTS)).then();
-		}
-	};
+// 			await router.push({
+// 				pathname: noticeCategory === 'FAQ' ? '/_admin/cs/faq' : '/_admin/cs/notice',
+// 				query: {
+// 					category: noticeCategory === 'FAQ' ? '/_admin/cs/faq' : ' /_admin/cs/notice',
+// 				},
+// 			});
+// 		} catch (err: any) {
+// 			console.log(err);
+// 			sweetErrorHandling(new Error(Message.INSERT_ALL_INPUTS)).then();
+// 		}
+// 	};
 
-	return (
-		<Stack>
-			<Stack direction="row" style={{ margin: '40px' }} justifyContent="space-evenly">
-				<Box component={'div'} className={'form_row'} style={{ width: '300px' }}>
-					<Typography style={{ color: '#7f838d', margin: '10px' }} variant="h3">
-						Category
-					</Typography>
-					<FormControl sx={{ width: '70%', background: 'white' }}>
-						<Select
-							value={noticeCategory}
-							onChange={changeCategoryHandler}
-							displayEmpty
-							inputProps={{ 'aria-label': 'Without label' }}
-						>
-							<MenuItem value={NoticeCategory.FAQ}>
-								<span>FAQ</span>
-							</MenuItem>
-							<MenuItem value={NoticeCategory.NOTICE}>NOTICE</MenuItem>
-						</Select>
-					</FormControl>
-				</Box>
+// 	return (
+// 		<Stack>
+// 			<Stack direction="row" style={{ margin: '40px' }} justifyContent="space-evenly">
+// 				<Box component={'div'} className={'form_row'} style={{ width: '300px' }}>
+// 					<Typography style={{ color: '#7f838d', margin: '10px' }} variant="h3">
+// 						Category
+// 					</Typography>
+// 					<FormControl sx={{ width: '70%', background: 'white' }}>
+// 						<Select
+// 							value={noticeCategory}
+// 							onChange={changeCategoryHandler}
+// 							displayEmpty
+// 							inputProps={{ 'aria-label': 'Without label' }}
+// 						>
+// 							<MenuItem value={NoticeCategory.FAQ}>
+// 								<span>FAQ</span>
+// 							</MenuItem>
+// 							<MenuItem value={NoticeCategory.NOTICE}>NOTICE</MenuItem>
+// 						</Select>
+// 					</FormControl>
+// 				</Box>
 
-				<Box component={'div'} className={'form_row'} style={{ width: '300px' }}>
-					<Typography style={{ color: '#7f838d', margin: '10px' }} variant="h3">
-						Status
-					</Typography>
-					<FormControl sx={{ width: '70%', background: 'white' }}>
-						<Select
-							value={noticeStatus}
-							onChange={changeStatusHandler}
-							displayEmpty
-							inputProps={{ 'aria-label': 'Without label' }}
-						>
-							<MenuItem value={NoticeStatus.ACTIVE}>
-								<span>ACTIVE</span>
-							</MenuItem>
-							<MenuItem value={NoticeStatus.HOLD}>HOLD</MenuItem>
-						</Select>
-					</FormControl>
-				</Box>
+// 				<Box component={'div'} className={'form_row'} style={{ width: '300px' }}>
+// 					<Typography style={{ color: '#7f838d', margin: '10px' }} variant="h3">
+// 						Status
+// 					</Typography>
+// 					<FormControl sx={{ width: '70%', background: 'white' }}>
+// 						<Select
+// 							value={noticeStatus}
+// 							onChange={changeStatusHandler}
+// 							displayEmpty
+// 							inputProps={{ 'aria-label': 'Without label' }}
+// 						>
+// 							<MenuItem value={NoticeStatus.ACTIVE}>
+// 								<span>ACTIVE</span>
+// 							</MenuItem>
+// 							<MenuItem value={NoticeStatus.HOLD}>HOLD</MenuItem>
+// 						</Select>
+// 					</FormControl>
+// 				</Box>
 
-				<Box component={'div'} className={'form_row'} style={{ width: '300px' }}>
-					<Typography style={{ color: '#7f838d', margin: '10px' }} variant="h3">
-						Type
-					</Typography>
-					<FormControl sx={{ width: '70%', background: 'white' }}>
-						<Select
-							value={noticeType}
-							onChange={changeTypeHandler}
-							displayEmpty
-							inputProps={{ 'aria-label': 'Without label' }}
-						>
-							<MenuItem value={NoticeType.PET}>
-								<span>PET</span>
-							</MenuItem>
-							<MenuItem value={NoticeType.COMMUNITY}>
-								<span>COMMUNITY</span>
-							</MenuItem>
+// 				<Box component={'div'} className={'form_row'} style={{ width: '300px' }}>
+// 					<Typography style={{ color: '#7f838d', margin: '10px' }} variant="h3">
+// 						Type
+// 					</Typography>
+// 					<FormControl sx={{ width: '70%', background: 'white' }}>
+// 						<Select
+// 							value={noticeType}
+// 							onChange={changeTypeHandler}
+// 							displayEmpty
+// 							inputProps={{ 'aria-label': 'Without label' }}
+// 						>
+// 							<MenuItem value={NoticeType.PET}>
+// 								<span>PET</span>
+// 							</MenuItem>
+// 							<MenuItem value={NoticeType.COMMUNITY}>
+// 								<span>COMMUNITY</span>
+// 							</MenuItem>
 
-							<MenuItem value={NoticeType.FORSELLERS}>
-								<span>FORSELLERS</span>
-							</MenuItem>
-							<MenuItem value={NoticeType.FORBUYERS}>
-								<span>FORBUYERS</span>
-							</MenuItem>
-							<MenuItem value={NoticeType.PAYMENT}>
-								<span>PAYMENT</span>
-							</MenuItem>
-							<MenuItem value={NoticeType.OTHER}>
-								<span>OTHER</span>
-							</MenuItem>
-						</Select>
-					</FormControl>
-				</Box>
-				<Box component={'div'} style={{ width: '300px', flexDirection: 'column' }}>
-					<Typography style={{ color: '#7f838d', margin: '10px' }} variant="h3">
-						Title
-					</Typography>
-					<TextField
-						onChange={articleTitleHandler}
-						id="filled-basic"
-						label="Type Title"
-						style={{ width: '300px', background: 'white' }}
-					/>
-				</Box>
-			</Stack>
+// 							<MenuItem value={NoticeType.FORSELLERS}>
+// 								<span>FORSELLERS</span>
+// 							</MenuItem>
+// 							<MenuItem value={NoticeType.FORBUYERS}>
+// 								<span>FORBUYERS</span>
+// 							</MenuItem>
+// 							<MenuItem value={NoticeType.PAYMENT}>
+// 								<span>PAYMENT</span>
+// 							</MenuItem>
+// 							<MenuItem value={NoticeType.OTHER}>
+// 								<span>OTHER</span>
+// 							</MenuItem>
+// 						</Select>
+// 					</FormControl>
+// 				</Box>
+// 				<Box component={'div'} style={{ width: '300px', flexDirection: 'column' }}>
+// 					<Typography style={{ color: '#7f838d', margin: '10px' }} variant="h3">
+// 						Title
+// 					</Typography>
+// 					<TextField
+// 						onChange={articleTitleHandler}
+// 						id="filled-basic"
+// 						label="Type Title"
+// 						style={{ width: '300px', background: 'white' }}
+// 					/>
+// 				</Box>
+// 			</Stack>
 
-			<Editor
-				initialValue={'Type here'}
-				placeholder={'Type here'}
-				previewStyle={'vertical'}
-				height={'200px'}
-				// @ts-ignore
-				initialEditType={'WYSIWYG'}
-				toolbarItems={[
-					['heading', 'bold', 'italic', 'strike'],
-					['ul', 'ol'],
-				]}
-				ref={editorRef}
-				events={{
-					load: function (param: any) {},
-				}}
-			/>
+// 			<Editor
+// 				initialValue={'Type here'}
+// 				placeholder={'Type here'}
+// 				previewStyle={'vertical'}
+// 				height={'200px'}
+// 				// @ts-ignore
+// 				initialEditType={'WYSIWYG'}
+// 				toolbarItems={[
+// 					['heading', 'bold', 'italic', 'strike'],
+// 					['ul', 'ol'],
+// 				]}
+// 				ref={editorRef}
+// 				events={{
+// 					load: function (param: any) {},
+// 				}}
+// 			/>
 
-			<Stack direction="row" justifyContent="center">
-				<Button
-					variant="contained"
-					color="primary"
-					style={{ margin: '20px', width: '200px', height: '45px' }}
-					onClick={handleRegisterButton}
-				>
-					Register
-				</Button>
-			</Stack>
-		</Stack>
-	);
-};
+// 			<Stack direction="row" justifyContent="center">
+// 				<Button
+// 					variant="contained"
+// 					color="primary"
+// 					style={{ margin: '20px', width: '200px', height: '45px' }}
+// 					onClick={handleRegisterButton}
+// 				>
+// 					Register
+// 				</Button>
+// 			</Stack>
+// 		</Stack>
+// 	);
+// };
 
-export default FAQTeditor;
+// export default FAQTeditor;
--- pages/_admin/cs/add/index.tsx
+++ pages/_admin/cs/add/index.tsx
@@ -1,75 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { NextPage } from 'next';
-import { Avatar, Box, Button, Paper, Stack, Typography } from '@mui/material';
-import dynamic from 'next/dynamic';
-import useDeviceDetect from '../../../../libs/hooks/useDeviceDetect';
-import { userVar } from '../../../../apollo/store';
-import '@toast-ui/editor/dist/toastui-editor.css';
-const FAQTeditor = dynamic(() => import('./FAQTeditor'), { ssr: false });
+// import React, { useEffect, useState } from 'react';
+// import { NextPage } from 'next';
+// import { Avatar, Box, Button, Paper, Stack, Typography } from '@mui/material';
+// import dynamic from 'next/dynamic';
+// import useDeviceDetect from '../../../../libs/hooks/useDeviceDetect';
+// import { userVar } from '../../../../apollo/store';
+// import '@toast-ui/editor/dist/toastui-editor.css';
+// const FAQTeditor = dynamic(() => import('./FAQTeditor'), { ssr: false });
 
-import { useReactiveVar } from '@apollo/client';
-import { useRouter } from 'next/router';
+// import { useReactiveVar } from '@apollo/client';
+// import { useRouter } from 'next/router';
 
-const WriteFAQ: NextPage = () => {
-	const device = useDeviceDetect();
-	const user = useReactiveVar(userVar);
-	const router = useRouter();
+// const WriteFAQ: NextPage = () => {
+// 	const device = useDeviceDetect();
+// 	const user = useReactiveVar(userVar);
+// 	const router = useRouter();
 
-	const [isEditorLoaded, setIsEditorLoaded] = useState(false);
+// 	const [isEditorLoaded, setIsEditorLoaded] = useState(false);
 
-	useEffect(() => {
-		setIsEditorLoaded(true);
-	}, []);
+// 	useEffect(() => {
+// 		setIsEditorLoaded(true);
+// 	}, []);
 
-	if (device === 'mobile') {
-		return <>ARTICLE PAGE MOBILE</>;
-	} else
-		return (
-			<div id="write-article-page">
-				<Stack className="main-title-box">
-					<Stack style={{ width: '70px', height: '70px', marginLeft: '65px' }} className={'logo-box'}>
-						<img src={'/img/logo/Logo.bg.png'} alt={'logo'} />
-					</Stack>
-					<Paper
-						elevation={1}
-						sx={{
-							display: 'flex',
-							alignItems: 'center',
-							padding: 2,
-							marginTop: 2,
-							borderRadius: 2,
-							backgroundColor: '#f9f9f9',
-							width: 'fit-content',
-						}}
-					>
-						<Avatar
-							src={
-								user?.memberImage
-									? `${process.env.REACT_APP_API_URL}/${user?.memberImage}`
-									: '/img/profile/defaultUser.svg'
-							}
-							sx={{ width: 48, height: 48, marginRight: 2 }}
-						/>
-						<Box>
-							<Typography variant="subtitle1" fontWeight="bold">
-								{user?.memberNick || 'Admin'}
-							</Typography>
-							<Typography variant="body2" color="textSecondary">
-								{user?.memberPhone || '+010345663333'}
-							</Typography>
-							<Button variant="contained" color="primary" onClick={() => router.push(`/_admin/`)}>
-								Clicl to Admin Page
-							</Button>
-						</Box>
-					</Paper>
-					<Stack flexDirection={'column'} alignItems={'center'} className="right-box">
-						<Typography className="main-title">Write FAQ/NOTICE</Typography>
-						<Typography className="sub-title">Write about Frequently Asked Questions and NOTICE </Typography>
-					</Stack>
-				</Stack>
-				<div>{isEditorLoaded ? <FAQTeditor /> : <div>Loading...</div>}</div>
-			</div>
-		);
-};
+// 	if (device === 'mobile') {
+// 		return <>ARTICLE PAGE MOBILE</>;
+// 	} else
+// 		return (
+// 			<div id="write-article-page">
+// 				<Stack className="main-title-box">
+// 					<Stack style={{ width: '70px', height: '70px', marginLeft: '65px' }} className={'logo-box'}>
+// 						<img src={'/img/logo/Logo.bg.png'} alt={'logo'} />
+// 					</Stack>
+// 					<Paper
+// 						elevation={1}
+// 						sx={{
+// 							display: 'flex',
+// 							alignItems: 'center',
+// 							padding: 2,
+// 							marginTop: 2,
+// 							borderRadius: 2,
+// 							backgroundColor: '#f9f9f9',
+// 							width: 'fit-content',
+// 						}}
+// 					>
+// 						<Avatar
+// 							src={
+// 								user?.memberImage
+// 									? `${process.env.REACT_APP_API_URL}/${user?.memberImage}`
+// 									: '/img/profile/defaultUser.svg'
+// 							}
+// 							sx={{ width: 48, height: 48, marginRight: 2 }}
+// 						/>
+// 						<Box>
+// 							<Typography variant="subtitle1" fontWeight="bold">
+// 								{user?.memberNick || 'Admin'}
+// 							</Typography>
+// 							<Typography variant="body2" color="textSecondary">
+// 								{user?.memberPhone || '+010345663333'}
+// 							</Typography>
+// 							<Button variant="contained" color="primary" onClick={() => router.push(`/_admin/`)}>
+// 								Clicl to Admin Page
+// 							</Button>
+// 						</Box>
+// 					</Paper>
+// 					<Stack flexDirection={'column'} alignItems={'center'} className="right-box">
+// 						<Typography className="main-title">Write FAQ/NOTICE</Typography>
+// 						<Typography className="sub-title">Write about Frequently Asked Questions and NOTICE </Typography>
+// 					</Stack>
+// 				</Stack>
+// 				<div>{isEditorLoaded ? <FAQTeditor /> : <div>Loading...</div>}</div>
+// 			</div>
+// 		);
+// };
 
-export default WriteFAQ;
+// export default WriteFAQ;
