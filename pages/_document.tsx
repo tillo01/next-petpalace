90af9cb8ca7c77cd7fe4dce4323c6aa8376114dd
Tillochik01 <xuribayevzakaryo@gmail.com>
2024-12-01 20:44:03 +0900

    fix: _document petpalace.uz fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ export default function Document() {
 					name={'description'}
 					content={
 						'Buy and sell pets anywhere anytime in Japan. Best pets at the best prices on petpalace.uz | ' +
-						'Покупайте и продавайте домашних животных в любой точке Японии в любое время. Лучшие питомцы по лучшим ценам на petpalace.jp | ' +
+						'Покупайте и продавайте домашних животных в любой точке Японии в любое время. Лучшие питомцы по лучшим ценам на petpalace.uz | ' +
 						'일본 어디서나 언제든지 애완동물을 사고팔 수 있습니다. petpalace.jp 최고의 가격으로 최고의 애완동물을 만나보세요.'
 					}
 				/>
